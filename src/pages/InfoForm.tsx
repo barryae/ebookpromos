26552ce13ec07e45dcd1e49499d40144dd66e894
Green Vine <barryaervin@gmail.com>
2020-04-14 12:16:13 -0400

    info page working

--- src/pages/InfoForm.tsx
+++ src/pages/InfoForm.tsx
@@ -1,27 +1,53 @@
 import * as React from 'react';
-import { Link } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
+import { useState, useRef } from 'react';
+import axios from 'axios';
 
-// const handleSubmit = (event: any) => {
-//     event.preventDefault()
 
-// }
 
-export default function InfoForm() {
+export default function InfoForm(this: any) {
+    const [toCheckout, setToCheckout] = useState(false)
+    const [ebookUrl, setUrl] = useState('')
+    const [tone, setTone] = useState('')
+    const [story, setStory] = useState('')
+
+    function handleChange(e: any) {
+        switch (e.target.name) {
+            case "ebookUrl":
+                setUrl(e.target.value)
+                break
+            case "tone":
+                setTone(e.target.value)
+                break
+            case "story":
+                setStory(e.target.value)
+                break
+        }
+    }
+
+    const handleSubmit = (e: any) => {
+        if (e) { e.preventDefault(); }
+        axios.post("https://submit-form.com/V3O5LOXxFB8gGpNGF0fRk", {
+            tone: tone,
+            story: story,
+            ebookUrl: ebookUrl,
+        }).then(res => { setToCheckout(true) })
+    }
+
+    // action = "https://submit-form.com/V3O5LOXxFB8gGpNGF0fRk" target = "_self"
+
     return (
         <div className='main'>
+            {toCheckout ? <Redirect to="/" /> : null}
             <h2>Tell Us About Your eBook</h2>
-            <form action="https://submit-form.com/V3O5LOXxFB8gGpNGF0fRk" target="_self">
+            <form onSubmit={handleSubmit}>
                 <p>What is the URL of your ebook on Amazon (or other online marketplaces)?</p>
-                <textarea name="ebookUrl" placeholder=""></textarea>
+                <textarea value={ebookUrl} name="ebookUrl" onChange={handleChange}></textarea>
                 <p>How would you describe the tone of your ebook? Please use three simple, descriptive words.</p>
-                <textarea name="tone" placeholder=""></textarea>
+                <textarea value={tone} name="tone" onChange={handleChange}></textarea>
                 <p>Please describe the story of your book in one sentence.</p>
-                <textarea name="story" placeholder=""></textarea>
-                <input type="hidden" name="_redirect" value="/checkout" />
-                <Link to="/checkout">
-                    <button type="submit">Submit</button>
-                </Link>
-
+                <textarea value={story} name="story" onChange={handleChange}></textarea>
+                <button type="submit">Submit</button>
             </form>
 
         </div >
