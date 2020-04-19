d1efe43657d7a1760bb29d62351c01083a8c5fce
Green Vine <barryaervin@gmail.com>
2020-04-19 12:49:02 -0400

    styling

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import Testimonial from "../components/Testimonial"
 import CallToAction from "../components/CallToAction"
+import AccentImage from "../components/AccentImage"
 
 
 let testimonialItems = [{ name: "Michael Alvear", book: "Revenge of the Hatless", url: "https://www.amazon.com/Revenge-Hatless-MAGA-Meets-Karma/dp/1795202858/ref=sr_1_1?dchild=1&keywords=revenge+of+the+hatless&qid=1586538158&sr=8-1", quote: 'eBook Promos produces videos that cut through the clutter to deliver winning messages and stellar sales.' }]
