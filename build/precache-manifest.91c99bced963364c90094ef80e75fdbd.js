58a29127ad3bc95ad4f1177fbbd994d25202ece5
Green Vine <barryaervin@gmail.com>
2020-04-28 12:15:32 -0400

    took hours out of readme

---
+++ build/precache-manifest.91c99bced963364c90094ef80e75fdbd.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ad66e67f11b290b017406531c3dbf40",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4dec4bf2a666ce7322f9",
+    "url": "/static/css/main.bf16a54a.chunk.css"
+  },
+  {
+    "revision": "01936f3a1edd3912fee9",
+    "url": "/static/js/2.fdae174e.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.fdae174e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4dec4bf2a666ce7322f9",
+    "url": "/static/js/main.44801125.chunk.js"
+  },
+  {
+    "revision": "99725bbe64ea1a09808e",
+    "url": "/static/js/runtime-main.7972a6c0.js"
+  },
+  {
+    "revision": "2cbf390bae5046bbc6ed07d430e0daeb",
+    "url": "/static/media/Privacy Policy.2cbf390b.pdf"
+  },
+  {
+    "revision": "057c952ad9065a59b79e25eb07d96d97",
+    "url": "/static/media/Terms of Use.057c952a.pdf"
+  },
+  {
+    "revision": "6a9ab7cf7deead7e30eefdbf41795e99",
+    "url": "/static/media/accentImage1.6a9ab7cf.jpg"
+  },
+  {
+    "revision": "a497e7d2ec9fc908fb76f4f8d1e7e74f",
+    "url": "/static/media/accentImage2.a497e7d2.jpg"
+  },
+  {
+    "revision": "42bb38db4edcff8798584759282dc7c7",
+    "url": "/static/media/logo 03.42bb38db.png"
+  },
+  {
+    "revision": "7eb595a0f8e2b9995559141784275615",
+    "url": "/static/media/project0.7eb595a0.png"
+  },
+  {
+    "revision": "717b8285edbb743486a9aee6bbb22510",
+    "url": "/static/media/stats.717b8285.png"
+  }
+]);
